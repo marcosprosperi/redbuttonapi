f22ae5d17df7f8137f9c467073bd7191e9b45052
Marcos Prosperi <mprosperi@gmail.com>
2020-04-10 15:32:36 -0300

    headers

--- src/index.js
+++ src/index.js
@@ -11,26 +11,8 @@ dotenv.config();
 const app = express();
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(function (req, res, next) {
 
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://serene-ptolemy-324869.netlify.com/')
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true)
-
-  // Pass to next layer of middleware
-  next()
-})
-
-app.use(cors())
+app.use(cors({origin: 'https://serene-ptolemy-324869.netlify.com/'}))
 
 
 
