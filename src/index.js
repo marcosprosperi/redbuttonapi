e7c2c22ae47fa77f92a8a976733162f00a99066c
Marcos Prosperi <mprosperi@gmail.com>
2020-04-10 15:36:04 -0300

    headers

--- src/index.js
+++ src/index.js
@@ -11,8 +11,12 @@ dotenv.config();
 const app = express();
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-
-app.use(cors({origin: 'https://serene-ptolemy-324869.netlify.com/'}))
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.use(cors())
 
 
 
