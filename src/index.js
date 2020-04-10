697a42e8331f4f23056cfc5071197b753961813b
Marcos Prosperi <mprosperi@gmail.com>
2020-04-10 15:49:13 -0300

    headers

--- src/index.js
+++ src/index.js
@@ -11,11 +11,6 @@ dotenv.config();
 const app = express();
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 app.use(cors())
 
 var port = process.env.PORT || 8080;
